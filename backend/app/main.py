989d85f1be58b842b85113ff98228241cd2209e2
Lion <mhrj.asis@gmail.com>
2024-01-07 17:42:26 +0100

    updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+# Get the current working directory
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+print(current_dir)
+
+# Append the project path to sys.path
+project_path = os.path.join(current_dir, "../..")  # Assuming your project is one level above the main script
+sys.path.append(project_path)
+
+
 from app.routers import (
     boottime,
     cpu,
--- backend/app/routers/whoami.py
+++ backend/app/routers/whoami.py
@@ -1,7 +1,7 @@
 __author__ = 'amaharjan.de'
 
 from fastapi import APIRouter
-import subprocess
+from utils.whoami import get_whoami
 from ..logger import log
 
 router = APIRouter()
@@ -12,9 +12,5 @@ def read_whoami():
     Return `whoami` from the OS.
     '''
     log.info('read_whoami started.')
-    try:
-        result = subprocess.check_output(['whoami'], text=True).strip()
-        return {'result': result}
-    except subprocess.CalledProcessError as e:
-        log.error(f'Error running whoami: {e}')
-        return {'error': 'Failed to run whoami.'}
+    result = get_whoami()
+    return {'result': result}
