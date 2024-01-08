f7cddbe05129fde4c28d350d1305bec03f06a4bd
Lion <mhrj.asis@gmail.com>
2024-01-08 15:00:52 +0100

    updates

--- django_project/tknight_django/settings.py
+++ django_project/tknight_django/settings.py
@@ -91,13 +91,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tknight_django.wsgi.application'
 
-
+print(project_path)
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':  f'{project_path}/db.sqlite3',
     }
 }
 
