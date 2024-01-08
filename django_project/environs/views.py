58481aa4c50e1c59733fc05b2929a3042dab60ea
Lion <mhrj.asis@gmail.com>
2024-01-08 06:34:36 +0100

    refactored

--- django_project/environs/views.py
+++ django_project/environs/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
-
+from utils.environ import get_environ
 
 class EnvironsTemplateView(TemplateView):
     extra_context = {'page_title': 'Environ'}
     template_name = 'environs/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['environs'] = get_environ()
+        return context
+    
