87b7437417aa6be8815f8b0ce599eb991c5ae7b4
Lion <mhrj.asis@gmail.com>
2024-01-08 07:12:11 +0100

    systemctl_services updated

--- django_project/system_services/views.py
+++ django_project/system_services/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
-
+from utils.systemctl_services import get_systemctl_services
 
 class SystemctlServicesTemplateView(TemplateView):
     extra_context = {'page_title': 'Systemctl Services'}
     template_name = 'system_services/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['systemctl_services'] = get_systemctl_services()
+        return context
