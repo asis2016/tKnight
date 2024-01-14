1dcdf85765df467c3cb65461d01e9a5611ee6352
Ashish S. Maharjan <hello@amaharjan.de>
2024-01-14 06:15:56 +0100

    user profile updated.

--- django_project/userprofile/views.py
+++ django_project/userprofile/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
+from utils.profile import get_profile
 
 
 class UserProfileTemplateView(TemplateView):
     extra_context = {'page_title': 'My Profile'}
     template_name = 'userprofile/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_profile()
+        return context
